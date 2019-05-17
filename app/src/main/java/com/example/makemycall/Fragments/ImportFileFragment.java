6a92599aa068cba8ffaa74e26c32e0ead71f056e
sohan <sohan.cse16@nituk.ac.in>
2019-05-18 01:13:12 +0530

    call can be connected to in the interval of 2 min to each listed contacts in the excel file

--- app/src/main/java/com/example/makemycall/Fragments/ImportFileFragment.java
+++ app/src/main/java/com/example/makemycall/Fragments/ImportFileFragment.java
@@ -26,6 +26,7 @@ import android.widget.Toast;
 
 import com.example.makemycall.Model.XYValue;
 import com.example.makemycall.R;
+import com.example.makemycall.Utill.MyContactFetch;
 import com.example.makemycall.Utill.SaveAndLoadContact;
 
 import org.apache.poi.hssf.usermodel.HSSFDateUtil;
@@ -89,7 +90,9 @@ public class ImportFileFragment extends Fragment {
 
                     //Execute method for reading the excel data.
 
-                    readExcelData(lastDirectory);
+
+                    MyContactFetch myContactFetch=new MyContactFetch(context);
+                    myContactFetch.execute(lastDirectory);
 
                 }else
                 {
@@ -128,104 +131,12 @@ public class ImportFileFragment extends Fragment {
         checkInternalStorage();
     }
 
-    /**
-     *reads the excel file columns then rows. Stores data as ExcelUploadData object
-     * @return
-     */
-    private void readExcelData(String filePath) {
-        Log.d(TAG, "readExcelData: Reading Excel File.");
-
-
-        HashMap<String,String> myContact=new HashMap<>();
-        //decarle input file
-        File inputFile = new File(filePath);
-
-        try {
-            InputStream inputStream = new FileInputStream(inputFile);
-            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            int rowsCount = sheet.getPhysicalNumberOfRows();
-            FormulaEvaluator formulaEvaluator = workbook.getCreationHelper().createFormulaEvaluator();
-            StringBuilder sb = new StringBuilder();
-
-            //outter loop, loops through rows
-            for (int r = 1; r < rowsCount; r++) {
-                Row row = sheet.getRow(r);
-                int cellsCount = row.getPhysicalNumberOfCells();
-                //inner loop, loops through columns
-                for (int c = 0; c < cellsCount; c++) {
-                    //handles if there are to many columns on the excel sheet.
-                    if(c>=2){
-                        Log.e(TAG, "readExcelData: ERROR. Excel File Format is incorrect! " );
-                        toastMessage("ERROR: Excel File Format is incorrect!");
-                        break;
-                    }else{
-                        String key = getCellAsString(row, c, formulaEvaluator);
-                        c++;
-                        String value=getCellAsString(row,c,formulaEvaluator);
-                        myContact.put(key,value);
-//                        String cellInfo = "r:" + r + "; c:" + c + "; v:" + value;
-//                        Log.d(TAG, "readExcelData: Data from row: " + cellInfo);
-                    }
-                }
-            }
-
-            //saving data into saredPreferance
-            SaveAndLoadContact saveAndLoadContact=new SaveAndLoadContact(context);
-            saveAndLoadContact.clearSharedPreferencs();
-            saveAndLoadContact.saveMap(myContact);
-
-
-        }catch (Exception e) {
-            Log.e(TAG, e.getMessage() );
-        }
-    }
-
-    /**
-     * Returns the cell as a string from the excel file
-     * @param row
-     * @param c
-     * @param formulaEvaluator
-     * @return
-     */
-    private String getCellAsString(Row row, int c, FormulaEvaluator formulaEvaluator) {
-        String value = "";
-        try {
-            Cell cell = row.getCell(c);
-            CellValue cellValue = formulaEvaluator.evaluate(cell);
-            switch (cellValue.getCellType()) {
-                case Cell.CELL_TYPE_BOOLEAN:
-                    value = ""+cellValue.getBooleanValue();
-                    break;
-                case Cell.CELL_TYPE_NUMERIC:
-                    double numericValue = cellValue.getNumberValue();
-                    if(HSSFDateUtil.isCellDateFormatted(cell)) {
-                        double date = cellValue.getNumberValue();
-                        SimpleDateFormat formatter =
-                                new SimpleDateFormat("MM/dd/yy");
-                        value = formatter.format(HSSFDateUtil.getJavaDate(date));
-                    } else {
-                        value = ""+numericValue;
-                    }
-                    break;
-                case Cell.CELL_TYPE_STRING:
-                    value = ""+cellValue.getStringValue();
-                    break;
-                default:
-            }
-        } catch (NullPointerException e) {
-
-            Log.e(TAG, "getCellAsString: NullPointerException: " + e.getMessage() );
-        }
-        return value;
-    }
-
     private void checkInternalStorage() {
         Log.d(TAG, "checkInternalStorage: Started.");
         try{
             if (!Environment.getExternalStorageState().equals(
                     Environment.MEDIA_MOUNTED)) {
-                toastMessage("No SD card found.");
+                Toast.makeText(context, "No SD card found.", Toast.LENGTH_SHORT).show();
             }
             else{
                 // Locate the image folder in your SD Car;d
@@ -283,9 +194,5 @@ public class ImportFileFragment extends Fragment {
      * customizable toast
      * @param message
      */
-    private void toastMessage(String message){
-        Toast.makeText(context,message, Toast.LENGTH_SHORT).show();
-    }
-
 
 }
